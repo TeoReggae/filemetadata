91967104f2833a0d5ef576e6a0db4388fb14ef02
U-DESKTOP-LKDJJP8\Teo <teoreggae5454@gmail.com>
2018-11-17 15:44:03 +0200

    madde it working

--- server.js
+++ server.js
@@ -2,7 +2,8 @@
 
 var express = require('express');
 var cors = require('cors');
-
+const multer = require('multer');
+var upload = multer();
 // require and use "multer"...
 
 var app = express();
@@ -13,6 +14,12 @@ app.use('/public', express.static(process.cwd() + '/public'));
 app.get('/', function (req, res) {
      res.sendFile(process.cwd() + '/views/index.html');
   });
+
+app.post('/api/fileanalyse', upload.single('upfile'), function(req, res){
+  res.json({name:req.file.originalname, type:req.file.mimetype, size:req.file.size});
+  console.log(req.file.originalname);
+
+});
 
 app.get('/hello', function(req, res){
   res.json({greetings: "Hello, API"});
